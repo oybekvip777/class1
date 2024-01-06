864ba1c7060881433d01cf9cb7c7b095c1069ff8
oybek777 <153380033+oybekvip777@users.noreply.github.com>
2024-01-06 13:47:43 +0500

    Add files via upload

---
+++ record1.13.py
@@ -0,0 +1,120 @@
+# 1-misol
+# class Fan():
+#     def __init__(self,nomi):
+#         self.nomi = nomi
+#         self.talabalar_soni = 0
+#         self.talabalar = []
+    
+#     def add_student(self,talaba):
+#         self.talabalar.append(talaba)
+#         self.talabalar_soni += 1
+    
+#     def get_name(self):
+#         return self.nomi
+    
+#     def get_students(self):
+#         return [x.get_info() for x in self.talabalar]
+    
+#     def get_students_num(self):
+#         return self.talabalar_soni
+    
+# 2A-misol
+# class AVTO():
+#     def __init__(self,ism,narxi,rang,model): 
+#         self.ism=ism
+#         self.narxi=narxi
+#         self.rang=rang
+#         self.model=model
+# avto1=AVTO('BMW',4500000,'OQ','I8')
+# print(avto1.ism)
+# print(avto1.model)
+# print(avto1.rang)
+# print(avto1.narxi)
+
+# 3A-misol
+# class AVTO():
+#     def __init__(self,ism,narxi,rang,model): 
+#         self.ism=ism
+#         self.narxi=narxi
+#         self.rang=rang
+#         self.model=model
+
+# def get_nema(self):
+#     return self.ism
+
+# def get_pay(self):
+#     return self.narxi
+
+# def get_color(self):
+#     return self.rang
+
+# def get_model(self):
+#     return self.model
+    
+# def get_all():
+#     print(f'moshina ism {get_nema} rangi {get_color} model esa {get_model} va narxi {get_pay} ')
+
+# 4A-misol
+
+# class AVTO():
+#     def __init__(self,ism,narxi,rang,model): 
+#         self.ism=ism
+#         self.narxi=narxi
+#         self.rang=rang
+#         self.model=model
+
+# def get_nema(self):
+#     return self.ism
+
+# def get_pay(self):
+#     return self.narxi
+
+# def get_color(self):
+#     return self.rang
+
+# def get_model(self):
+#     return self.model
+    
+# def get_info():
+#     print(f'moshina ism {get_nema} rangi {get_color} model esa {get_model} va narxi {get_pay} ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
